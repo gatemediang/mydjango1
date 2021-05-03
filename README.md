4e2dfe72c1310f4799f2c6bca6d48b68e1f29357
gatemediang <estolog@gmail.com>
2021-05-03 08:58:39 +0100

    new commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mydjango1
+
 A Simple Django App Newly Installed App
+
+## How To Run Application
+
+CD into mydjango1 folder and run below commands
+`python manage.py runserver
---
+++ firstdjango_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FirstdjangoAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'firstdjango_app'
